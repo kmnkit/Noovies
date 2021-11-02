76d57aaa2e502290fd3fb9999a80e71643403bc1
Marco Kang <kmnkit@gmail.com>
2021-11-02 22:00:09 +0900

    tab bar icons

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import AppLoading from "expo-app-loading";
-import React, { useState } from 'react';
+import React from 'react';
 import * as Font from 'expo-font';
-import { Text, Image } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import { Asset, useAssets } from 'expo-asset';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native'; // DarkTheme, DefaultTheme 커스텀 안한 다크, 라이트 모드 사용 가능
 import Tabs from "./navigation/Tabs";
+
 
 export default function App() {
   const [assets] = useAssets([require('./casa.jpg')]);
   const [loaded] = Font.useFonts(Ionicons.font); // useFonts는 fonts 로드 여부에 따라 T or F를 준다.
+
   if (!assets || !loaded) {
     return <AppLoading />;
   }
+  // return <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}><Tabs /></NavigationContainer>
   return <NavigationContainer><Tabs /></NavigationContainer>
 }
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,8 +3,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Movie from '../screens/Movie';
 import Tv from '../screens/Tv';
 import Search from '../screens/Search';
-import { View, Text, useColorScheme } from 'react-native';
+import { useColorScheme } from 'react-native';
 import { DOUBLE_ORAGON_SKIN, IMPERIAL_PRIMER, LIGHT_BLUE_BALERINA, STORM_PETREL } from '../colors';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -24,15 +25,33 @@ const Tabs = () => {
                 },
                 headerTitleStyle: {
                     color: isDark ? DOUBLE_ORAGON_SKIN : IMPERIAL_PRIMER,
+                },
+                tabBarLabelStyle: {
+                    marginTop: -5,
+                    fontSize: 12,
+                    fontWeight: "600"
                 }
             }}
         >
             <Tab.Screen
                 name="Movies"
                 component={Movie}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Ionicons name="film-outline" color={color} size={size} />
+                }}
             />
-            <Tab.Screen name="TV" component={Tv} options={{ tabBarBadge: 5 }} />
-            <Tab.Screen name="Search!" component={Search} />
+            <Tab.Screen
+                name="TV"
+                component={Tv}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Ionicons name="tv-outline" color={color} size={size} />
+                }} />
+            <Tab.Screen
+                name="Search"
+                component={Search}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Ionicons name="search-outline" color={color} size={size} />
+                }} />
         </Tab.Navigator>)
 };
 
