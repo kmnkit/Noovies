8f2b4918e7e7bccdecd1ec098fdebec791f2af47
Marco Kang <kmnkit@gmail.com>
2021-11-02 21:30:09 +0900

    dark mode

---
+++ colors.js
@@ -0,0 +1,7 @@
+// Reference
+// https://flatuicolors.com/palette/ca
+
+export const IMPERIAL_PRIMER = "#222f3e";
+export const STORM_PETREL = "#8395a7";
+export const LIGHT_BLUE_BALERINA = "#c8d6e5";
+export const DOUBLE_ORAGON_SKIN = "#ff9f43";
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,29 +3,37 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Movie from '../screens/Movie';
 import Tv from '../screens/Tv';
 import Search from '../screens/Search';
-import { View, Text } from 'react-native';
+import { View, Text, useColorScheme } from 'react-native';
+import { DOUBLE_ORAGON_SKIN, IMPERIAL_PRIMER, LIGHT_BLUE_BALERINA, STORM_PETREL } from '../colors';
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-    // initialRouteName는 기본 선택되어 있는 탭을 설정함
-    // <Tab.Navigator initialRouteName="Search">
-    <Tab.Navigator>
-        <Tab.Screen
-            name="Movies"
-            component={Movie}
-            options={{
-                headerTitleStyle: { color: 'tomato' },
-                headerRight: () => (
-                    <View>
-                        <Text>Hello</Text>
-                    </View>
-                )
+const Tabs = () => {
+    const isDark = useColorScheme() === 'dark';
+
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: {
+                    backgroundColor: isDark ? IMPERIAL_PRIMER : LIGHT_BLUE_BALERINA
+                },
+                tabBarActiveTintColor: isDark ? DOUBLE_ORAGON_SKIN : IMPERIAL_PRIMER,
+                tabBarInactiveTintColor: isDark ? LIGHT_BLUE_BALERINA : STORM_PETREL,
+                headerStyle: {
+                    backgroundColor: isDark ? IMPERIAL_PRIMER : LIGHT_BLUE_BALERINA
+                },
+                headerTitleStyle: {
+                    color: isDark ? DOUBLE_ORAGON_SKIN : IMPERIAL_PRIMER,
+                }
             }}
-        />
-        <Tab.Screen name="TV" component={Tv} options={{ tabBarBadge: 5 }} />
-        <Tab.Screen name="Search!" component={Search} />
-    </Tab.Navigator>
-);
+        >
+            <Tab.Screen
+                name="Movies"
+                component={Movie}
+            />
+            <Tab.Screen name="TV" component={Tv} options={{ tabBarBadge: 5 }} />
+            <Tab.Screen name="Search!" component={Search} />
+        </Tab.Navigator>)
+};
 
 export default Tabs;
