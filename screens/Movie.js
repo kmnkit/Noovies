9414a928656c1b31fe66d99b07af1e76424ac8cb
Marco Kang <kmnkit@gmail.com>
2021-11-07 15:44:24 +0900

    dirty nowplaying

--- screens/Movie.js
+++ screens/Movie.js
@@ -1,21 +1,42 @@
 import React from "react";
 import styled from "styled-components/native";
+import { Dimensions } from "react-native";
+import Swiper from "react-native-web-swiper";
 
-const Btn = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+const API_KEY = "0eaa6db7e6ac99943d7b4b24e95ec968";
+
+const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
 
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
+const View = styled.View`
+  flex: 1;
 `;
 
-const Movie = ({ navigation: { navigate } }) => (
-  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
-    <Title>Movie</Title>
-  </Btn>
-);
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const Movie = () => {
+  const getNowPlaying = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=ko-KR&page=1&region=KR`
+    );
+  };
+
+  return (
+    <Container>
+      <Swiper
+        loop
+        timeout={3}
+        controlsEnabled={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+      </Swiper>
+    </Container>
+  );
+};
 
 export default Movie;
